2067e9128f7e552554d92f4e038393891bedc298
hadi.banaee <hadi.banaee@oru.se>
2026-04-24 12:58:29 +0200

    config: set default steps to 500

--- domains/dock_loading/actions.py
+++
@@ -1,7 +0,0 @@
-# domains/pallet_shop/actions.py
-"""
-Action operator definitions for the pallet_shop domain.
-TODO: implement actual preconditions/effects for these actions.
-"""
-
-# TODO: Check Kitting tasks for reference on how to structure these ActionOperators, then implement actual actions for pallet_shop domain.
