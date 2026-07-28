849f15e12a43dc3c7a42c19d74eaa6cf498fcb43
hadi.banaee <hadi.banaee@oru.se>
2026-07-28 13:03:02 +0200

    kitting: ?kitting_table/?coffee_machine/?ac_switch typed params

--- domains/kitting/tasks.py
+++ domains/kitting/tasks.py
@@ -8,19 +8,23 @@ from shared.types import Var, Const, ConditionSchema, StepCall, MethodSchema, Ta
 from domains.kitting.actions import pick_up, move_to, place, wait_at
 
 _item      = Var("?item")
+_kitting_table = Var("?kitting_table")
+_coffee_machine = Var("?coffee_machine")
+_ac_switch = Var("?ac_switch")
+
+# target and entity are used in move_to and wait_at actions, respectively.
 _target    = Var("?target")
 _entity    = Var("?entity")
 
-# TODO: later Var("?coffee_machine") and Var("?ac_switch") for more flexible task definitions, 
-# if needed. For now, hardcoding to specific constant objects for simplicity.
 
 deliver_item = TaskSchema(
     name="deliver_item",
-    parameters=[_item],
+    parameters=[_item, _kitting_table],
+    parameter_types={"?item": "item", "?kitting_table": "kitting_table"},
     methods=[
         MethodSchema(
             name="deliver_default",
-            parameters=[_item],
+            parameters=[_item, _kitting_table],
             guards=[],
             step_calls=[
                 StepCall(
@@ -33,11 +37,11 @@ deliver_item = TaskSchema(
                 ),
                 StepCall(
                     action_name="move_to",
-                    bindings={_target: Const("kitting_table")},
+                    bindings={_target: _kitting_table}
                 ),
                 StepCall(
                     action_name="place",
-                    bindings={_item: _item, _target: Const("kitting_table")},
+                    bindings={_item: _item, _target: _kitting_table},
                 ),
             ],
         )
@@ -48,20 +52,21 @@ deliver_item = TaskSchema(
 
 coffee_break = TaskSchema(
     name="coffee_break",
-    parameters=[],
+    parameters=[_coffee_machine],
+    parameter_types={"?coffee_machine": "coffee_machine"},
     methods=[
         MethodSchema(
             name="coffee_break_default",
-            parameters=[],
+            parameters=[_coffee_machine],
             guards=[],
             step_calls=[
                 StepCall(
                     action_name="move_to",
-                    bindings={_target: Const("coffee_machine_0")},
+                    bindings={_target: _coffee_machine},
                 ),
                 StepCall(
                     action_name="wait_at",
-                    bindings={_entity: Const("coffee_machine_0"), Var("?duration"): Const("PT60S")},   # using processcompletion 
+                    bindings={_entity: _coffee_machine, Var("?duration"): Const("PT60S")},   # using processcompletion 
                 ),
             ],
         )
@@ -72,20 +77,21 @@ coffee_break = TaskSchema(
 
 ac_activation = TaskSchema(
     name="ac_activation",
-    parameters=[],
+    parameters=[_ac_switch],
+    parameter_types={"?ac_switch": "ac_switch"},
     methods=[
         MethodSchema(
             name="ac_activation_default",
-            parameters=[],
+            parameters=[_ac_switch],
             guards=[],
             step_calls=[
                 StepCall(
                     action_name="move_to",
-                    bindings={_target: Const("ac_switch_0")},
+                    bindings={_target: _ac_switch},
                 ),
                 StepCall(
                     action_name="wait_at",
-                    bindings={_entity: Const("ac_switch_0"), Var("?duration"): Const("PT2S")},  # using processcompletion
+                    bindings={_entity: _ac_switch, Var("?duration"): Const("PT2S")},  # using processcompletion
                 ),
             ],
         )
